9d57c9925636abe091d852f7b92b50c15f49dd14
barathkumaran2003 <barathkumaran2003@gmail.com>
2025-09-13 20:08:02 +0530

    Backen files

--- src/main/java/com/teched/app/config/GlobalCorsConfig.java
+++ src/main/java/com/teched/app/config/GlobalCorsConfig.java
@@ -1,11 +1,21 @@
 package com.teched.app.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class GlobalCorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        // Enable CORS for your API endpoints
+        registry.addMapping("/api/**")
+                .allowedOrigins("http://localhost:3000", "http://localhost:62439") // frontend origins
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*");
+    }
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
